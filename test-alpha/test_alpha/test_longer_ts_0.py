82ac1b8b734c668abb58dceea2350ed2a1b12f3f
LiXu <hugh@contek.io>
2024-03-14 14:27:20 +0800

    adfsdsfa

--- test-alpha/test_alpha/test_longer_ts_0.py
+++ test-alpha/test_alpha/test_longer_ts_0.py
@@ -13,16 +13,7 @@ class AlphaLongerTs0(AlphaSumTs0):
     """
 
     def __init__(self,
-                 universe,
-                 datasets_exchange_symbols=None,
-                 obj_id=(0,),
-                 params=None):
-        AlphaSumTs0.__init__(
-            self,
-            universe=universe,
-            datasets_exchange_symbols=datasets_exchange_symbols,
-            obj_id=obj_id,
-            params=params)
+                 
 
         self.run = 0
 
