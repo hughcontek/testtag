e4a16c06fd075504969aa5877c2a91ec24f370f5
LiXu <hugh@contek.io>
2024-03-14 14:26:36 +0800

    afsd

--- test-alpha/test_alpha/test_raise_ts_0 - 副本.py
+++ test-alpha/test_alpha/test_raise_ts_0 - 副本.py
@@ -9,17 +9,6 @@ class AlphaRaiseTs0(AlphaBase):
     def set_default_params(self):
         pass
 
-    def __init__(self,
-                 universe,
-                 datasets_exchange_symbols=None,
-                 obj_id=(0,),
-                 params=None):
-        super().__init__(
-            universe=universe,
-            datasets_exchange_symbols=datasets_exchange_symbols,
-            obj_id=obj_id,
-            params=params)
-
         self.info('Construction starts')
 
         self.info('Construction completed')
